a64124234afabc17e21243aa681b4965ee8cb795
ayaen <git@ayaen.xyz>
2025-11-19 23:31:06 +0530

    feat(app): register routes for new admin pages

--- CPANEL/src/App.tsx
+++ CPANEL/src/App.tsx
@@ -11,6 +11,19 @@ import RfidList from './pages/rfid/RfidList'
 import ReservationList from './pages/reservations/ReservationList'
 import Operations from './pages/operations/Operations'
 import UserList from './pages/users/UserList'
+// New imports
+import PartnerList from './pages/partners/PartnerList'
+import LocationList from './pages/locations/LocationList'
+import EvDriverList from './pages/ev-drivers/EvDriverList'
+import TransactionList from './pages/transactions/TransactionList'
+import ScheduleList from './pages/schedules/ScheduleList'
+import DowntimeList from './pages/downtime/DowntimeList'
+import MapList from './pages/maps/MapList'
+import TariffList from './pages/tariffs/TariffList'
+import StaticDataList from './pages/static-data/StaticDataList'
+import ConnectionList from './pages/connections/ConnectionList'
+import ServerLogList from './pages/server-logs/ServerLogList'
+import QrGenerator from './pages/qr-generator/QrGenerator'
 
 function App() {
   const { isAuthenticated } = useAuthStore()
@@ -36,6 +49,20 @@ function App() {
         <Route path="/reservations" element={<ReservationList />} />
         <Route path="/operations" element={<Operations />} />
         <Route path="/users" element={<UserList />} />
+        {/* New routes */}
+        <Route path="/partners" element={<PartnerList />} />
+        <Route path="/locations" element={<LocationList />} />
+        <Route path="/ev-drivers" element={<EvDriverList />} />
+        <Route path="/transactions" element={<TransactionList />} />
+        <Route path="/schedules" element={<ScheduleList />} />
+        <Route path="/downtime" element={<DowntimeList />} />
+        <Route path="/maps" element={<MapList />} />
+        <Route path="/charging-sessions" element={<SessionList />} />
+        <Route path="/tariffs" element={<TariffList />} />
+        <Route path="/static-data" element={<StaticDataList />} />
+        <Route path="/connections" element={<ConnectionList />} />
+        <Route path="/server-logs" element={<ServerLogList />} />
+        <Route path="/qr-generator" element={<QrGenerator />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </AdminLayout>
