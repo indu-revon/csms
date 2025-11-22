2a51799c6df2f4d612638115b5af5e7b95143714
mark-ab <subashokbhosale@gmail.com>
2025-11-22 15:56:38 +0530

    feat(frontend): Setup React Query provider
    - Wrap application with QueryClientProvider
    - Configure default query options:
      - Disable refetch on window focus
      - Set retry attempts to 1
    - Enable React Query caching throughout the app

--- CPANEL/src/main.tsx
+++ CPANEL/src/main.tsx
@@ -2,24 +2,37 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { ConfigProvider, App as AntApp } from 'antd'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App'
 import './index.css'
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+    },
+  },
+})
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ConfigProvider
-        theme={{
-          token: {
-            colorPrimary: '#1890ff',
-            borderRadius: 6,
-          },
-        }}
-      >
-        <AntApp>
-          <App />
-        </AntApp>
-      </ConfigProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ConfigProvider
+          theme={{
+            token: {
+              colorPrimary: '#1890ff',
+              borderRadius: 6,
+            },
+          }}
+        >
+          <AntApp>
+            <App />
+          </AntApp>
+        </ConfigProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
+
